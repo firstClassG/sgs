55c87e5a9961106b868ec8034520cf1e386eafa3
Jack Hsieh <jack@jackhsieh.com>
2015-03-24 14:18:27 -0700

    Move code to header and footer file

---
+++ footer.php
@@ -0,0 +1,8 @@
+<?php
+if (!isset($_SESSION['user'])) {
+  header('Location: login.php');
+}
+?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,41 @@
+<?php
+if (!isset($_SESSION['user'])) {
+  header('Location: login.php');
+}
+
+function displayAdminLink() {
+  if(isset($_SESSION['user']['admin'])) {
+    echo '<li><a href="admin.php">Admin</a></li>';
+  }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="css/style.css" />
+
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  <script src="js/script.js"></script>
+</head>
+<body>
+  <div class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand">Satellite Ground Station</a>
+      </div>
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Dashboard</a></li>
+          <li><a href="search.php">Search</a></li>
+          <?php displayAdminLink(); ?>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </div>
+  </div>
